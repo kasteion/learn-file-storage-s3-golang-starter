dfdbdf546c0ab79e9e618e5deb95baa1e768b575
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2025-09-29 15:27:47 -0600

    CH2: Caching - New Files

--- assets.go
+++ assets.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -16,9 +16,15 @@ func (cfg apiConfig) ensureAssetsDir() error {
 	return nil
 }
 
-func getAssetPath(videoID uuid.UUID, mediaType string) string {
+func getAssetPath(mediaType string) string {
+	randomBytes := make([]byte, 32)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		panic("failed to generate random bytes")
+	}
+	fileName := base64.RawURLEncoding.EncodeToString(randomBytes)
 	ext := mediaTypeToExt(mediaType)
-	return fmt.Sprintf("%s%s", videoID, ext)
+	return fmt.Sprintf("%s%s", fileName, ext)
 }
 
 func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
